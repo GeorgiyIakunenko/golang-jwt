d435c4353d9fd4256b868aed58d01fff0cc38588
Georgiy Iakunenko <goshanyakunenko@gmail.com>
2023-05-11 12:59:22 +0200

    go database 1.0

---
+++ internal/helpers/helper.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+var formatting = []byte{',', '\n'}
+
+func Create(filename string, entity interface{}) error {
+	jsonBytes, err := json.Marshal(entity)
+	if err != nil {
+		return err
+	}
+
+	jsonBytes = append(jsonBytes, '\n')
+
+	file, err := os.OpenFile(fmt.Sprintf("internal/repos/file_repository/datastore/%s.txt", filename), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer file.Close()
+
+	_, err = file.Write(jsonBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
